29934c8d0b653451ded2247278b44e6537cd1f43
Stephanie Cao <pcao@powerpuff.cs.brynmawr.edu>
2017-05-02 15:39:55 -0400

    kinda done w fopen?

--- fslib.cpp
+++ fslib.cpp
@@ -5,46 +5,35 @@
 vnode_t *root;
 
 //have a find function to get vnode pointer as helper for f_open, f_remove, other funcs with directories
-Vnode* findVnode(vnode_t *vn, string path){
-  
-}
 
-
-fd_t f_open(vnode_t *vn, string path, int flag){
+fd_t f_open(string path, int flag){
   int inRoot = 0;
+  Vnode* currFile;
   vector<string> names = new vector<string>();
   //parse path by "/" or "\", save names in vector
   //if first elem parsed == "~" set inRoot to 1 & not add it to vector
   parseLine(path, names); //check if parseLine func parses by \ or /
 
-  //int curadd;
-  int numchild;
-  
+  int numchild;  
   int start = 0;
-  //create g_vnode *current to store currently visited vnode
   if (names[i].compare("~") == 0){
     start = 1;
-    //curadd = BLOCKSIZE * rootreg;
-    //lseek(g_disk_fd, curadd, SEEK_SET);
-    //read(g_disk_fd, current, sizeof(Vnode));
     current = root;
-  } else {
-    //lseek(g_disk_fd, BLOCKSIZE * current->fatPtr);
   }
   
   //follow path
   int fileExist = 0;
   for (int i = start; i < names.size(); i++){
     int validDir = 0;
-    if (current->type == 0 && current->size > 0){
+    if (current->type == 1 && current->size > 0){
       int numchild = current->size;
       int curPtr = current->fatPtr;
-      while (curPtr != -1){
+      while (curPtr != USMAX){
 	for (int j = 0; j < 7; i++){
 	  if (numchild == 0) break;
 	  numchild--;
 
-	  char* nodename;
+	  char nodename[255];
 	  lseek(g_disk_fd, BLOCKSIZE * curPtr + j * 288, SEEK_SET);
 	  read(g_disk_fd, nodename, 255);
 	  if (names[j].compare(nodename) == 0){
@@ -55,18 +44,6 @@ fd_t f_open(vnode_t *vn, string path, int flag){
 	    validDir = 1;
 	    break;
 	  }
-
-	  /* Vnode* newnode;
-	  lseek(g_disk_fd, BLOCKSIZE * curPtr + j * 288, SEEK_SET);
-	  read(g_disk_fd, newnode, sizeof(Vnode)); //change read in size
-	  //need to rearrange fields in Vnode
-	  if (names[j].compare(newnode->name) == 0){
-	    *(current->children).add(newnode);
-	    current = newnode;
-	    validDir = 1;
-	    break;
-	  }
-	  */
 	}
 	curPtr = g_FAT_table[curPtr];
       }
@@ -74,6 +51,8 @@ fd_t f_open(vnode_t *vn, string path, int flag){
       //not a directory
       if (i == names.size()-1){
 	//found file, file's vnode stored in current
+	currFile = current;
+	current = current->parent; //reset current to the parent dir
 	fileExist = 1;
 	break;
       } else {
@@ -90,11 +69,10 @@ fd_t f_open(vnode_t *vn, string path, int flag){
   }
 
   if (fileExist == 0){
-    //create new file (new inode etc)
     //need to: edit permisssion, traverse for fat_ptr
     int newBlock = -1;
     for (int i = 0; i < FATSIZE; i++){
-      if (g_FAT_table[i] == -2){ //agree on indices in FAT for empty blocks
+      if (g_FAT_table[i] == USMAX){ //agree on indices in FAT for empty blocks
 	newBlock = i;
 	break;
       }
@@ -106,21 +84,25 @@ fd_t f_open(vnode_t *vn, string path, int flag){
     }
 
     //do we need id??
-    Vnode *newVnode = (Vnode*) malloc (sizeof(Vnode));
+    currFile = (Vnode*) malloc (sizeof(Vnode));
     newVnode->name = names.back();
     //uid, gid, permission, timestamp
     newVnode->size = 0;
     newVnode->parent = current;
-    *(newVnode->children) = new vector<Vnode*>*();
     newVnode->type = 0;
     newVnode->fatPtr = newBlock;
-    *(current->children).push_back(&newVnode);
     //write to file
   }
 
   //add file to filetable
-  //traverse from beginning or last inserted index?
-  int i;
+  int idx = getNextIndex();
+  if (idx < 0){
+    //error message -- file table full
+    return -1;
+  }
+  g_file_table.addFileEntry(FtEntry(idx, currFile, 0, flag));
+  
+  /* int i;
   for (i = 0; i < MAXFTSIZE; i++){
     if (ftable[i].index == -1){
       ftable[i].index = i;
@@ -138,6 +120,7 @@ fd_t f_open(vnode_t *vn, string path, int flag){
     //error message -- file table full
     return -1;
   }
+  */
 
   return i;
 }
@@ -162,10 +145,7 @@ int f_close(fd_t fd){
   free(ftable[fd].vnode);
   
   //insert empty entry into file table
-  ftable[fd].index = -1;
-  ftable[fd].vnode = NULL;
-  ftable[fd].offset = -1;
-  ftable[fd].flag = -1;
+  g_file_table.removeFileEntry(FtEntry());
 
   return 0;
 }
